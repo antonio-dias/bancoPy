2076eb71a87a8ee440ae7fcb6536c12982549227
Antonio Dias <antonio_g_d@hotmail.com>
2021-09-30 16:08:14 -0300

    Primeiro commit

---
+++ banco.py
@@ -0,0 +1,73 @@
+from typing import List
+from time import sleep
+
+from models.cliente import Cliente
+from models.conta import Conta
+
+contas: List[Conta] = []
+
+
+def main() -> None:
+    menu()
+
+
+def menu() -> None:
+    print('======================================')
+    print('===============    ATM    ============')
+    print('=============== Geek Bank ============')
+    print('Selecione uma opção no menu: ')
+    print('1 - Criar conta')
+    print('2 - Efetuar saque')
+    print('3 - Efetuar depósito')
+    print('4 - Efetuar transferência')
+    print('5 - Listar contas')
+    print('6 - Sair do sistema')
+
+    opcao: int = int(input())
+
+    if opcao == 1:
+        criar_conta()
+    elif opcao == 2:
+        efetuar_saque()
+    elif opcao == 3:
+        efetuar_deposito()
+    elif opcao == 4:
+        efetuar_transferencia()
+    elif opcao == 5:
+        listar_contas()
+    elif opcao == 6:
+        print('Volte sempre')
+        sleep(2)
+        exit(0)
+    else:
+        print('Opção inválida')
+        sleep(2)
+        menu()
+
+
+def criar_conta() -> None:
+    pass
+
+
+def efetuar_saque() -> None:
+    pass
+
+
+def efetuar_deposito() -> None:
+    pass
+
+
+def efetuar_transferencia() -> None:
+    pass
+
+
+def listar_contas() -> None:
+    pass
+
+
+def buscar_conta_por_numero(numero: int) -> Conta:
+    pass
+
+
+if __name__ == '__main__':
+    main()
